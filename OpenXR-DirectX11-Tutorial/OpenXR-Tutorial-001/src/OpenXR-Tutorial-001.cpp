258cc71ef8b262fe578a6905f079343fe96e8d72
Ashley Matheson <ashley.matheson@unity3d.com>
2023-02-08 20:48:22 -0800

    More Output Information after adding minimal D3D initialization

--- OpenXR-DirectX11-Tutorial/OpenXR-Tutorial-001/src/OpenXR-Tutorial-001.cpp
+++ OpenXR-DirectX11-Tutorial/OpenXR-Tutorial-001/src/OpenXR-Tutorial-001.cpp
@@ -37,6 +37,61 @@ INITIALIZE_EASYLOGGINGPP\
 #include <iostream>
 
 
+IDXGIAdapter1* GetAdapter(LUID& adapterLUID)
+{
+    // find the appropriate DXGI adapter give a specific adapter LUID
+    IDXGIAdapter1* foundAdapter = nullptr;
+    IDXGIAdapter1* currentAdapter = nullptr;
+    IDXGIFactory1* dxgiFactory;
+    DXGI_ADAPTER_DESC1 adapterDescription;
+
+    CreateDXGIFactory1(__uuidof(IDXGIFactory1), (void**)(&dxgiFactory));
+
+    int curr = 0;
+    while (dxgiFactory->EnumAdapters1(curr++, &currentAdapter) == S_OK)
+    {
+        currentAdapter->GetDesc1(&adapterDescription);
+
+        if (memcmp(&adapterDescription.AdapterLuid, &adapterLUID, sizeof(&adapterLUID)) == 0)
+        {
+            foundAdapter = currentAdapter;
+            break;
+        }
+        currentAdapter->Release();
+        currentAdapter = nullptr;
+    }
+    dxgiFactory->Release();
+    return foundAdapter;
+}
+
+const char* XrSpaceToString(XrReferenceSpaceType spaceType)
+{
+    const char* result;
+    switch (spaceType)
+    {
+    case XR_REFERENCE_SPACE_TYPE_VIEW:
+        result = "View";
+        break;
+    case XR_REFERENCE_SPACE_TYPE_LOCAL:
+        result = "Local";
+        break;
+    case XR_REFERENCE_SPACE_TYPE_STAGE:
+        result = "Stage";
+        break;
+    case XR_REFERENCE_SPACE_TYPE_UNBOUNDED_MSFT:
+        result = "Unbounded MSFT";
+        break;
+    case XR_REFERENCE_SPACE_TYPE_COMBINED_EYE_VARJO:
+        result = "Combined Eye Varjo";
+        break;
+    default:
+        result = "Undefined";
+        break;
+    }
+
+    return result;
+}
+
 
 bool LogExtensions()
 {
@@ -102,6 +157,87 @@ void LogApiLayers()
 	}
 }
 
+bool LogXRInstance(XrResult xrResult, XrInstance instance)
+{
+    XrInstanceProperties xrInstanceProperties{ XR_TYPE_INSTANCE_PROPERTIES };
+    xrResult = xrGetInstanceProperties(instance, &xrInstanceProperties);
+
+    if (xrResult == XR_SUCCESS)
+    {
+        XrVersion runtimeVerstion = xrInstanceProperties.runtimeVersion;
+        LOG(INFO) << "XR Instance runtime name: " << xrInstanceProperties.runtimeName << " runtime version:" << XR_VERSION_MAJOR(runtimeVerstion) << ":" << XR_VERSION_MINOR(runtimeVerstion) << ":" << XR_VERSION_PATCH(runtimeVerstion);
+    }
+
+    return xrResult == XR_SUCCESS;
+}
+
+bool LogSystemInfoAndProperties(XrResult xrResult, XrInstance instance, XrSystemId& systemID)
+{
+    XrSystemGetInfo systemGetInfo{ XR_TYPE_SYSTEM_GET_INFO };
+    systemGetInfo.formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY;
+    xrResult = xrGetSystem(instance, &systemGetInfo, &systemID);
+    if (xrResult == XR_SUCCESS)
+    {
+        LOG(INFO) << "For Form Factor: " << systemGetInfo.formFactor << " Got System ID: " << systemID;
+    }
+
+    XrSystemProperties systemProperties{ XR_TYPE_SYSTEM_PROPERTIES };
+    xrResult = xrGetSystemProperties(instance, systemID, &systemProperties);
+    if (xrResult == XR_SUCCESS)
+    {
+        LOG(INFO) << "System Name: " << systemProperties.systemName << " Vendor ID: " << systemProperties.vendorId;
+        LOG(INFO) << "Max Swapchain Image Width: " << systemProperties.graphicsProperties.maxSwapchainImageWidth << " Height: " << systemProperties.graphicsProperties.maxSwapchainImageHeight << " Layer Count: " << systemProperties.graphicsProperties.maxLayerCount;
+        LOG(INFO) << "Orientation Tracking: " << (systemProperties.trackingProperties.orientationTracking == XR_TRUE ? "Enabled" : "Disabled") << " Position Tracking: " << (systemProperties.trackingProperties.positionTracking == XR_TRUE ? "Enabled" : "Disabled");
+    }
+
+    return xrResult == XR_SUCCESS;
+}
+
+bool LogXRSpaces(XrSession session, XrResult& xrResult)
+{
+    uint32_t spaceCount = 0;
+    xrEnumerateReferenceSpaces(session, 0, &spaceCount, nullptr);
+
+    if (spaceCount > 0)
+    {
+        std::vector<XrReferenceSpaceType> spaces(spaceCount);
+        xrResult = xrEnumerateReferenceSpaces(session, spaceCount, &spaceCount, spaces.data());
+        if (xrResult != XR_SUCCESS)
+        {
+            LOG(ERROR) << "Failed to enumerate the reference spaces";
+            return false;
+        }
+
+        for (auto refSpace : spaces)
+        {
+            LOG(INFO) << "Reference Space: " << XrSpaceToString(refSpace);
+        }
+    }
+
+	return true;
+}
+
+bool CreateXRSession(ID3D11Device* d3dDevice, XrResult& xrResult, XrInstance instance, XrSession& session, XrSystemId systemID)
+{
+	// To get more data, we need to create a session
+	// To create the session, we must identify a valid display subsystem
+	XrGraphicsBindingD3D11KHR binding = { XR_TYPE_GRAPHICS_BINDING_D3D11_KHR };
+	binding.device = d3dDevice;
+
+	XrSessionCreateInfo sessionCreateInfo{ XR_TYPE_SESSION_CREATE_INFO };
+	sessionCreateInfo.systemId = systemID;
+	sessionCreateInfo.next = &binding;
+
+	xrResult = xrCreateSession(instance, &sessionCreateInfo, &session);
+	if (xrResult != XR_SUCCESS)
+	{
+		LOG(ERROR) << "Failed to create the XR Session";
+		return false;
+	}
+
+	return true;
+}
+
 int main()
 {
 	// What do we have for available extensions
@@ -111,8 +247,16 @@ int main()
 	LogApiLayers();
 
 	// So we don't get a crash on exit, we need to create and destroy the XR Instance.
+    const char* necessaryExtensions[] =
+	{
+        XR_KHR_D3D11_ENABLE_EXTENSION_NAME, // Use Direct3D11 for rendering
+        XR_EXT_DEBUG_UTILS_EXTENSION_NAME,  // Debug utils for extra info
+    };
+
 	XrInstanceCreateInfo xrInstanceCreateInfo{ XR_TYPE_INSTANCE_CREATE_INFO };
 	xrInstanceCreateInfo.applicationInfo.apiVersion = XR_CURRENT_API_VERSION;
+	xrInstanceCreateInfo.enabledExtensionCount = 2;
+	xrInstanceCreateInfo.enabledExtensionNames = necessaryExtensions;
 	strcpy_s(xrInstanceCreateInfo.applicationInfo.applicationName, "Tutorial");
 	XrInstance instance;
 
@@ -124,59 +268,51 @@ int main()
 	}
 
 	// Tell us about the XR Instance
-	XrInstanceProperties xrInstanceProperties {XR_TYPE_INSTANCE_PROPERTIES};
-	xrResult = xrGetInstanceProperties(instance, &xrInstanceProperties);
-	if (xrResult == XR_SUCCESS)
-	{
-		XrVersion runtimeVerstion = xrInstanceProperties.runtimeVersion;
-		LOG(INFO) << "XR Instance runtime name: " << xrInstanceProperties.runtimeName << " runtime version:" << XR_VERSION_MAJOR(runtimeVerstion) << ":" << XR_VERSION_MINOR(runtimeVerstion) << ":" << XR_VERSION_PATCH(runtimeVerstion);
-	}
+	LogXRInstance(xrResult, instance);
 
 	// Tell us something about the system
-	XrSystemGetInfo systemGetInfo {XR_TYPE_SYSTEM_GET_INFO};
-	systemGetInfo.formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY;
-	XrSystemId systemID;
-	xrResult = xrGetSystem(instance, &systemGetInfo, &systemID);
-	if (xrResult == XR_SUCCESS)
-	{
-		LOG(INFO) << "For Form Factor: " << systemGetInfo.formFactor << " Got System ID: " << systemID;
-	}
+    XrSystemId systemID;
+	LogSystemInfoAndProperties(xrResult, instance, systemID);
 
-	XrSystemProperties systemProperties {XR_TYPE_SYSTEM_PROPERTIES};
-	xrResult = xrGetSystemProperties(instance, systemID, &systemProperties);
-	if (xrResult == XR_SUCCESS)
-	{
-		LOG(INFO) << "System Name: " << systemProperties.systemName << " Vendor ID: "<< systemProperties.vendorId;
-	    LOG(INFO) << "Max Swapchain Image Width: " << systemProperties.graphicsProperties.maxSwapchainImageWidth << " Height: " << systemProperties.graphicsProperties.maxSwapchainImageHeight << " Layer Count: " <<systemProperties.graphicsProperties.maxLayerCount;
-		LOG(INFO) << "Orientation Tracking: " << (systemProperties.trackingProperties.orientationTracking == XR_TRUE ? "Enabled" : "Disabled") << " Position Tracking: " << (systemProperties.trackingProperties.positionTracking == XR_TRUE ? "Enabled" : "Disabled");
-	}
+	// Initialize DirectX so we can get a D3D Device to play with
+	// First off, we need to get the function pointer to the xrGetD3D11GraphicsRequirementsKHR extension
+	PFN_xrGetD3D11GraphicsRequirementsKHR xrGetD3D11GraphicsRequirementsKHREXT = nullptr;
+	xrResult = xrGetInstanceProcAddr(instance, "xrGetD3D11GraphicsRequirementsKHR", (PFN_xrVoidFunction*)(&xrGetD3D11GraphicsRequirementsKHREXT));
 
-	// To get more data, we need to create a session
-	// To create the session, we must identify a valid display subsystem
-	XrSession session;
+    XrGraphicsRequirementsD3D11KHR requirement = { XR_TYPE_GRAPHICS_REQUIREMENTS_D3D11_KHR };
+    xrGetD3D11GraphicsRequirementsKHREXT(instance, systemID, &requirement);
 
-	XrSessionCreateInfo sessionCreateInfo {XR_TYPE_SESSION_CREATE_INFO};
-	sessionCreateInfo.systemId = systemID;
-	xrResult = xrCreateSession(instance, &sessionCreateInfo, &session);
-	if (xrResult != XR_SUCCESS)
-	{
-		LOG(ERROR) << "Failed to create the XR Session";
+    ID3D11Device*			d3dDevice = nullptr;
+    ID3D11DeviceContext*	d3dContext = nullptr;
+
+    IDXGIAdapter1* adapter = GetAdapter(requirement.adapterLuid);
+    D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0 };
+
+    if (adapter == nullptr)
+    {
+		LOG(ERROR) << "Failed to create the DXGI Adapter";
+        return -1;
+    }
+
+    if (FAILED(D3D11CreateDevice(adapter, D3D_DRIVER_TYPE_UNKNOWN, 0, 0, featureLevels, _countof(featureLevels), D3D11_SDK_VERSION, &d3dDevice, nullptr, &d3dContext)))
+    {
+		LOG(ERROR) << "Failed to create the D3D11 Device";
+        return -1;
+    }
+
+    adapter->Release();
+
+    XrSession session;
+
+    if (!CreateXRSession(d3dDevice, xrResult, instance, session, systemID))
 		return -1;
-	}
-	// What spaces does the hardware support?
-	uint32_t spaceCount = 0;
-	xrEnumerateReferenceSpaces(session, 0, &spaceCount, nullptr);
 
-	if (spaceCount > 0)
-	{
-		std::vector<XrReferenceSpaceType> spaces(spaceCount);
-		xrResult = xrEnumerateReferenceSpaces(session, spaceCount, &spaceCount, spaces.data());
-		if (xrResult != XR_SUCCESS)
-		{
-			LOG(ERROR) << "Failed to enumerate the reference spaces";
-			return -1;
-		}
-	}
+    // What spaces does the hardware support?
+	if (!LogXRSpaces(session, xrResult))
+		return -1;
+
+	d3dContext->Release();
+	d3dDevice->Release();
 	xrDestroySession(session);
 	xrDestroyInstance(instance);
 
